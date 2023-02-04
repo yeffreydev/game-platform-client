823065ddcfa24073208ba8cb70dfaa2b8c8d9371
yeffreycode <yeffreyespinoza200@gmail.com>
2023-02-04 15:36:46 -0500

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,8 @@
+import react from 'react';
+
+const Home = () =>{
+return <div>
+</div>
+}
+
+export default Home;
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import {Routes, Route} from 'react-router-dom';
+import SignIn from './../pages/SignIn';
+
+const Router = () => {
+    return <Routes>
+            <Route path='/login' component={<SignIn/>} />
+            <Route path='/register' component={<SignIn />} />
+        </Routes>
+}
+
+export default Router;
