48ea0e988df3965378b06e6254e4edaedfba7ded
yeffreycode <yeffreyespinoza200@gmail.com>
2023-02-06 08:57:27 -0500

    [FT/002 src/ & login and register component

--- src/components/signIn/Login.tsx
+++ src/components/signIn/Login.tsx
@@ -1,7 +1,30 @@
 import React from "react";
-
+import styles from "./../../styles/signIn/Login.module.css";
 const Login = () => {
-  return <div></div>;
+  return (
+    <form className={styles.login}>
+      <div className={styles.form_header}>
+        <h2>Welcome back</h2>
+        <p>you and me.</p>
+      </div>
+      <div className={styles.form_group}>
+        <label htmlFor=""> email</label>
+        <input type="text" />
+      </div>
+      <div className={styles.form_group}>
+        <label htmlFor="">password</label>
+        <input type="text" />
+      </div>
+      <div className={styles.form_group}>
+        <button type="submit">Login</button>
+      </div>
+      <div className={styles.form_footer}>
+        <p>
+          Need an Account? <b>Register</b>
+        </p>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
--- src/components/signIn/Register.tsx
+++ src/components/signIn/Register.tsx
@@ -1,7 +1,35 @@
 import React from "react";
-
+import loginStyles from "./../../styles/signIn/Login.module.css";
 const Register = () => {
-  return <div></div>;
+  return (
+    <form className={loginStyles.login}>
+      <div className={loginStyles.form_header}>
+        <h2> create an account</h2>
+        <p>more time with your friends.</p>
+      </div>
+      <div className={loginStyles.form_group}>
+        <label htmlFor=""> email</label>
+        <input type="text" />
+      </div>
+      <div className={loginStyles.form_group}>
+        <label htmlFor=""> username</label>
+        <input type="text" />
+      </div>
+      <div className={loginStyles.form_group}>
+        <label htmlFor="">password</label>
+        <input type="text" />
+      </div>
+      <div className={loginStyles.form_group}>
+        <button type="submit">Register</button>
+      </div>
+      <div className={loginStyles.form_footer}>
+        <p>
+          already have an Account? <b>Login</b>
+        </p>
+        <p>terms of service and privacy policy</p>
+      </div>
+    </form>
+  );
 };
 
 export default Register;
